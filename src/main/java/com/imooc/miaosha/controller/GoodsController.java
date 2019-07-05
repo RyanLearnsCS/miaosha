c32276445ce67d89cc32a2ddf39741341493e5ae
Ryan <36127337+RyanLearnsCS@users.noreply.github.com>
2019-07-05 10:31:35 +0800

    chapter3

--- src/main/java/com/imooc/miaosha/controller/GoodsController.java
+++ src/main/java/com/imooc/miaosha/controller/GoodsController.java
@@ -3,7 +3,9 @@ package com.imooc.miaosha.controller;
 import com.imooc.miaosha.domain.MiaoshaUser;
 import com.imooc.miaosha.domain.User;
 import com.imooc.miaosha.redis.RedisService;
+import com.imooc.miaosha.service.GoodsService;
 import com.imooc.miaosha.service.MiaoshaUserService;
+import com.imooc.miaosha.vo.GoodsVo;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,10 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Controller
 @RequestMapping("/goods")
@@ -25,6 +29,9 @@ public class GoodsController {
 
     @Autowired
     RedisService redisService;
+
+    @Autowired
+    GoodsService goodsService;
 
 
     private static Logger logger = LoggerFactory.getLogger(GoodsController.class);
@@ -42,7 +49,40 @@ public class GoodsController {
         if (user != null) {
             model.addAttribute("user", user);
         }
+        List<GoodsVo> goodsList = goodsService.listGoodsVo();
+        model.addAttribute("goodsList", goodsList);
         return "goods_list";
+    }
+
+
+    @RequestMapping("/to_detail/{goodsId}")
+    public String detail(Model model, MiaoshaUser user, @PathVariable("goodsId") long goodsId) {
+        model.addAttribute("user", user);
+
+        GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
+        model.addAttribute("goods", goods);
+
+        //
+        long startAt = goods.getStartDate().getTime();
+        long endAt = goods.getEndDate().getTime();
+        long now = System.currentTimeMillis();
+
+        int miaoshaStatus = 0;
+        int remainSeconds = 0;
+
+        if (now < startAt) { //秒杀还没开始，倒计时
+            miaoshaStatus = 0;
+            remainSeconds = (int) (startAt - now) / 1000;
+        } else if (now > endAt){ //秒杀已经结束
+            miaoshaStatus = 2;
+            remainSeconds = -1;
+        } else { //秒杀进行中
+            miaoshaStatus = 1;
+            remainSeconds = 0;
+        }
+        model.addAttribute("miaoshaStatus", miaoshaStatus);
+        model.addAttribute("remainSeconds", remainSeconds);
+        return "goods_detail";
     }
 
 /*
@@ -59,5 +99,7 @@ public class GoodsController {
         }
         return "goods_list";
     }*/
+
+
 
 }
